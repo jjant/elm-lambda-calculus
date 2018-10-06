e302115cef1b117fafbe674200a6ec9eadf8fc17
jjant <jantonielli@itba.edu.ar>
2018-10-07 00:13:31 +0200

    Add some pretty printing to output JS

--- src/Compiler.elm
+++ src/Compiler.elm
@@ -5,11 +5,11 @@ import Lambda exposing (Expr(..), Lit(..))
 
 compile : Expr -> String
 compile e =
-    "(" ++ toJs e ++ ")"
+    "(" ++ toJs 1 e ++ ")"
 
 
-toJs : Expr -> String
-toJs expr =
+toJs : Int -> Expr -> String
+toJs indent expr =
     case expr of
         Lit (LInt i) ->
             String.fromInt i
@@ -21,20 +21,23 @@ toJs expr =
             string
 
         Lam arg expr1 ->
-            function arg (toJs expr1)
+            function indent arg (toJs (indent + 1) expr1)
 
         App expr1 expr2 ->
-            "(" ++ toJs expr1 ++ ")" ++ "(" ++ toJs expr2 ++ ")"
+            "(" ++ toJs indent expr1 ++ ")" ++ "(" ++ toJs indent expr2 ++ ")"
 
 
-function : String -> String -> String
-function arg body =
+function : Int -> String -> String -> String
+function indent arg body =
     let
+        tabs =
+            String.repeat indent "\t"
+
         buildArgs a =
             " (" ++ a ++ ") "
 
         buildBody b =
-            "{" ++ "\n\treturn " ++ body ++ ";" ++ "\n}"
+            "{" ++ "\n" ++ tabs ++ "\t" ++ "return " ++ body ++ ";" ++ "\n" ++ tabs ++ "}"
     in
         "function " ++ buildArgs arg ++ buildBody body
 
